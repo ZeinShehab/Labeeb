a262c31f0952ba0ae5711fe7e3f885018d848373
Zein Shehab <zeinshehab@outlook.com>
2023-10-22 14:48:56 +0300

    Create script to properly name images and labels

--- rename_files.py
+++ rename_files.py
@@ -1,13 +1,13 @@
 import os
 
-
-images_dir = "D:/archive-unaug/unaugmented/416/train/images"
-labels_dir = "D:/archive-unaug/unaugmented/416/train/labels"
+data_dir = "D:/new-archive/datasets/train"
+images_dir = f"{data_dir}/images"
+labels_dir = f"{data_dir}/labels"
 
 labels = os.listdir(labels_dir)
 images = os.listdir(images_dir)
 
-image_count = [-1 for _ in range(28)]
+image_count = [-1 for _ in range(32)]
 
 
 for image in images:
@@ -21,22 +21,14 @@ for image in images:
 
     if label_index >= 0:
         image_index = int(open(os.path.join(labels_dir, label_name), "r").read().split(' ')[0])
-        
-        if (image_index == 0 and image_count[image_index] == 464):
-            image_index = 24
+
+        # Special for dataset that mislabeled the 24th symbol as 0        
+        # if (image_index == 0 and image_count[image_index] == 464):
+        #     image_index = 24
         
         image_count[image_index] += 1
 
         os.rename(os.path.join(images_dir, f"{image_name}.jpg"), os.path.join(images_dir, f"{image_index}_{image_count[image_index]}.jpg"))
         os.rename(os.path.join(labels_dir, label_name), os.path.join(labels_dir, f"{image_index}_{image_count[image_index]}.txt"))
 
-
 print(image_count)
-
-# with open("labels.csv", "w+") as csv:
-#     for filename in (os.listdir("labels"))[::-1]:
-#         datafile = open(os.path.join("labels", filename), "r")
-#         # csv.write(datafile.read() + "\n")
-
-#         print(datafile.read().split(' ')[0])
-#         datafile.close()
