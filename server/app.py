dca6add3428a5711ff294f71e113bd668646abd4
Zein Shehab <zeinshehab@outlook.com>
2024-02-09 14:35:40 +0200

    Fixed issue with server

--- server/app.py
+++ server/app.py
@@ -1,45 +1,34 @@
 from flask import Flask, jsonify, request
-import pandas as pd
 import numpy as np
-import joblib
 import traceback
 import cv2 as cv
 import copy
 import mediapipe as mp
-from flask_restful import reqparse
-import itertools
 app = Flask(__name__)
-import xgboost as xgb
 from xgboost import XGBClassifier
-import pickle
 import sys
-sys.path.append('D:/LSL_Translator/LSL_Translator')
+sys.path.append('C:/Users/zeins/LSL_Translator/')
 from helpers.generate_keypoints import calc_landmark_list
 
-xgb_save_path = "../model/keypoint_classifier.pkl"
+xgb_save_path = "../model/alphabet_classifier.pkl"
+# xgb_save_path = "../model/keypoint_classifier.pkl"
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # model = pickle.load(open(xgb_save_path, "rb"))
-    # model = joblib.load("../model/keypoint_classifier.pkl")
     model = XGBClassifier()
     model.load_model(xgb_save_path)
     if model:
         try:
             file = request.files['image']
             filestr = file.read()
-            #convert string data to numpy array
             file_bytes = np.fromstring(filestr, np.uint8)
-            # convert numpy array to image
             image = cv.imdecode(file_bytes, cv.IMREAD_UNCHANGED)
-            # print(type(file))
-            # image = cv.imread(file)
             image = cv.flip(image, 1)  # Mirror display
-            image = cv.rotate(image, cv.ROTATE_90_CLOCKWISE)
-            # cv.imshow("App Image", image)
-            # cv.waitKey(0)
-            debug_image = copy.deepcopy(image)
+            # image = cv.rotate(image, cv.ROTATE_90_CLOCKWISE)
+
             # Detection implementation #############################################################
+            debug_image = copy.deepcopy(image)
+
             image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
             image.flags.writeable = False
@@ -51,20 +40,25 @@ def predict():
                 min_detection_confidence=0.5,
                 min_tracking_confidence=0.5,
             )
+
+            # cv.imshow("image", image)
+            # cv.waitKey(0)
+
             results = hands.process(image)
             image.flags.writeable = True
 
             #  ####################################################################
             if results.multi_hand_landmarks is not None:
+                # print("[+] HAND DETECTED!")
                 for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
                                                     results.multi_handedness):
                     # Landmark calculation
                     pred_landmarks = calc_landmark_list(debug_image, hand_landmarks)
                     pred_landmarks = np.array(pred_landmarks)
                     pred_landmarks = pred_landmarks.reshape(1, -1)
+
                     pred = model.predict(pred_landmarks)
 
-                    print(pred[0])
                 return jsonify({'prediction': int(pred[0])})
             else:
                 return jsonify({'prediction': -1})
@@ -73,23 +67,6 @@ def predict():
     else:
         return ('No model here to use')
     
-
-def calc_pred_landmarks(image, landmarks):
-    image_width, image_height = image.shape[1], image.shape[0]
-
-    landmark_point = []
-
-    # Keypoint
-    for _, landmark in enumerate(landmarks.landmark):
-        # landmark_x = min(int(landmark.x * image_width), image_width - 1)
-        # landmark_y = min(int(landmark.y * image_height), image_height - 1)
-        landmark_x = float(landmark.x)
-        landmark_y = float(landmark.y)
-        landmark_z = float(landmark.z)
-
-        landmark_point.append([landmark_x, landmark_y, landmark_z])
-
-    return landmark_point
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
