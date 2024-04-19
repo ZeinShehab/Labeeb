e48acc1918a6561ebbdc1b5aaa1e649722c7f027
ZeinShehab <zeinshehab@outlook.com>
2024-04-19 11:14:34 +0300

    Fixed and tested gesture classification model

--- src/lsl_translator/test/test.py
+++ src/lsl_translator/test/test.py
@@ -1,9 +1,13 @@
 from lsl_translator.helpers.mediapipe_helper import MediaPipe
 import cv2 as cv
 import os
+import numpy as np
+
+from xgboost import XGBClassifier
+model_save_path = 'C:/Users/zeinshehab/LSL_Translator/src/lsl_translator/model/gesture_classifier.pkl'
 
 mp = MediaPipe()
-path = "D:/capstone-datasets/multi_hand_gestures/test/"
+path = "C:/Users/zeinshehab/LSL_Translator/images/temp/1"
 data = os.listdir(path)
 
 print(data)
@@ -19,4 +23,16 @@ for image in images:
     print(mp.contains_landmarks(image))
 
 gesture_landmarks = mp.get_multi_hand_landmarks_gesture(images)
-print(gesture_landmarks)
+# print(gesture_landmarks)
+
+loaded_model = XGBClassifier()
+loaded_model.load_model(model_save_path)
+
+xs = np.array(gesture_landmarks)
+xs = xs.reshape((1, -1))
+
+
+prediction = loaded_model.predict(xs)
+p = loaded_model.predict_proba(xs)
+print(prediction)
+print(p)
